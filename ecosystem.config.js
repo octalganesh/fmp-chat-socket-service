af2726740fd042913901fb4b07465f8f7278d15c
octalganesh <114572621+octalganesh@users.noreply.github.com>
2025-11-26 15:52:01 +0530

    Update MongoDB URIs and add UAT environment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,22 +11,22 @@ module.exports = {
         NODE_ENV: "development",
         PORT: 3000,
         MONGO_URI:
-          "mongodb+srv://arpitkhandelwal_db_user:mSSAp6svVcTVwdVY@chatcluster.68s0tan.mongodb.net/development-chat-db?retryWrites=true&w=majority",
+          "mongodb+srv://hardikkhurana_db_user:kVjmZLi9bSzgYVDS@cluster0.ckgioeb.mongodb.net/development-chat-db?retryWrites=true&w=majority",
         JWT_SECRET: "fsm",
       },
       env_staging: {
         NODE_ENV: "staging",
         PORT: 3001,
         MONGO_URI:
-          "mongodb+srv://arpitkhandelwal_db_user:mSSAp6svVcTVwdVY@chatcluster.68s0tan.mongodb.net/staging-chat-db?retryWrites=true&w=majority",
+          "mongodb+srv://hardikkhurana_db_user:kVjmZLi9bSzgYVDS@cluster0.ckgioeb.mongodb.net/staging-chat-db?retryWrites=true&w=majority",
         JWT_SECRET: "fsm",
       },
 
-      env_production: {
-        NODE_ENV: "production",
+      env_uat: {
+        NODE_ENV: "uat",
         PORT: 3000,
         MONGO_URI:
-          "mongodb+srv://arpitkhandelwal_db_user:mSSAp6svVcTVwdVY@chatcluster.68s0tan.mongodb.net/production-chat-db?retryWrites=true&w=majority",
+          "mongodb+srv://hardikkhurana_db_user:kVjmZLi9bSzgYVDS@cluster0.ckgioeb.mongodb.net/uat-chat-db?retryWrites=true&w=majority",
         JWT_SECRET: "fsm",
         instances: "max",
         exec_mode: "cluster",
