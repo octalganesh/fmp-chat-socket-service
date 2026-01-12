45a8d2e6e2439a07631d8394dd3af15a223801ac
ganeshoctal <ganesh.prajapati@octalsoftware.com>
2026-01-12 15:44:06 +0530

    fsdfsdf

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -3,14 +3,16 @@ const Message = require("../models/message");
 const getChatHistory = async (req, res) => {
 
   const { otherUserId, taskId } = req.params;
+  console.log("task id", taskId);
   const userId = req.user.id;
   try {
     const messages = await Message.find({
       $or: [
-        { sender: userId, receiver: otherUserId,taskId:taskId },
-        { sender: otherUserId, receiver: userId,taskId:taskId },
+        { sender: userId, receiver: otherUserId, taskId: taskId },
+        { sender: otherUserId, receiver: userId, taskId: taskId },
       ],
     }).sort({ createdAt: 1 });
+    console.log("messages", messages);
     // .populate("sender", "username")
     // .populate("receiver", "username");
     res.json(messages);
